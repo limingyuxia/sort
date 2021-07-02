8438c7d16571cc61312de78d35b8f6081bb55f16
v_vmingli <2572597150@qq.com>
2021-07-02 11:49:27 +0800

    add comments

--- main.go
+++ main.go
@@ -3,29 +3,29 @@ package main
 import (
 	"fmt"
 	// "sort/bubbleSort"
-	// "sort/insertionSort"
-	// "sort/quickSort"
-	// "sort/selectionSort"
-	// "sort/hillSort"
-	// "sort/mergeSort"
-	// "sort/heapSort"
-	// "sort/countingSort"
 	// "sort/bucketSort"
-	"sort/radixSort"
+	// "sort/countingSort"
+	// "sort/heapSort"
+	// "sort/hillSort"
+	// "sort/insertionSort"
+	// "sort/mergeSort"
+	// "sort/quickSort"
+	// "sort/radixSort"
+	// "sort/selectionSort"
 )
 
 func main() {
 	arr := []int{45, 78, 4, 9, 54, 34, 23, 66, 10}
 
 	fmt.Println(arr)
-	// fmt.Println(quickSort.QuickSort(arr))
-	// fmt.Println(bubbleSort.BubbleSort(arr))
-	// fmt.Println(selectionSort.SelectionSort(arr))
-	// fmt.Println(insertionSort.InsertionSort(arr))
-	// fmt.Println(hillSort.HillSort(arr))
-	// fmt.Println(mergeSort.MergeSort(arr))
-	// fmt.Println(heapSort.HeapSort(arr))
-	// fmt.Println(countingSort.CountingSort(arr, 78))
-	// fmt.Println(bucketSort.BucketSort(arr))
-	fmt.Println(radixSort.RadixSort(arr))
+	// fmt.Println(bubbleSort.BubbleSort(arr))         // 冒泡排序
+	// fmt.Println(selectionSort.SelectionSort(arr))   // 选择排序
+	// fmt.Println(insertionSort.InsertionSort(arr))   // 插入排序
+	// fmt.Println(hillSort.HillSort(arr))             // 希尔排序
+	// fmt.Println(mergeSort.MergeSort(arr))           // 归并排序
+	// fmt.Println(quickSort.QuickSort(arr))           // 快速排序
+	// fmt.Println(heapSort.HeapSort(arr))             // 堆排序
+	// fmt.Println(countingSort.CountingSort(arr, 78)) // 计数排序
+	// fmt.Println(bucketSort.BucketSort(arr))         // 桶排序
+	// fmt.Println(radixSort.RadixSort(arr))           // 基数排序
 }
